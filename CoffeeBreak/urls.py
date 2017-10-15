ac15412c0f367bb4c18d14e6c2b1fddca9e228fe
helio-luk <helio.ssjr@gmail.com>
2017-10-15 13:50:31 -0200

    Historia de usuario 1 - criacao de usuario

--- CoffeeBreak/urls.py
+++ CoffeeBreak/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- CoffeeBreak/views.py
+++ CoffeeBreak/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UsuarioForm
+from django.shortcuts import redirect
 
 # Create your views here.
-def index(request):
-    return HttpResponse("index do site")
+def index(request):#isso aqui tá bem errado, é pra mudar
+    return render(request, 'CoffeeBreak/base.html')
+
+def post_new(request):
+    if request.method == "POST":
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('index')
+    else:
+        form = UsuarioForm()
+    return render(request, 'CoffeeBreak/new_user.html', {'form': form})
