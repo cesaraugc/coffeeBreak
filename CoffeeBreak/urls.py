e7568120e737d1753533745385b82685f1aa2dd1
cesaraugc <cesaraugc@gmail.com>
2017-11-26 18:50:05 -0200

    correcao bug dashboard

--- CoffeeBreak/urls.py
+++ CoffeeBreak/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index', views.index, name='index'),
     url(r'^cadastro/$', views.post_new, name='post_new'),
     url(r'^login/$', views.loginView, name= 'loginView'),
     url(r'^logout/$', views.logoutView, name= 'logoutView'),
