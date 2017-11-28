c3b17287d642f5441b7544809f366a74ae02f9db
Jonatas Senna <jonrsp96@gmail.com>
2017-11-27 22:15:43 -0200

    testes

--- CoffeeBreak/tests.py
+++ CoffeeBreak/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import unittest
+import datetime
+from CoffeeBreak.models import Usuario
 
 # Create your tests here.
+class UsuarioTestCase(unittest.TestCase):
+    def setUp(self):
+        self.joao = Usuario.objects.create(nome_usuario= "joao", nome="joao1", senha="12345", email="joao@gmail.com")
+
+    def testSpeaking(self):
+        self.assertEquals(self.joao.nome_usuario, "joao")
+        self.assertEquals(self.joao.nome, "joao1")
+        self.assertEquals(self.joao.senha, "12345")
+        self.assertEquals(self.joao.email, "joao@gmail.com")
